d9e4a49d9ee4b5b7f597ae83b607bed43cdf500b
Adrian-Ureche <adrian_ctin20@yahoo.com>
2021-10-12 17:29:08 +0300

    Adrian Ureche - L02 datc

---
+++ Ureche Constantin-Adrian/L02/Models/Student.cs
@@ -0,0 +1,13 @@
+namespace Models
+{
+    public class Student
+    {
+        public int Id {get; set;}
+        public string Nume {get; set;}
+        public string Prenume {get; set;}
+        public string Facultate {get; set;}
+        public int An {get; set;}
+
+    }
+
+}
