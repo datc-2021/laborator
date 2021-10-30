1690125ea88b1aa26cfefb35287c12e5ab1a93b6
Daniel Horț <dnielhort@gmail.com>
2021-10-30 15:16:36 +0300

    Homework L04 done

---
+++ Hort Daniel/L04/L04/StudentEntity.cs
@@ -0,0 +1,16 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace L04;
+
+public class StudentEntity : ITableEntity
+{
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Faculty { get; set; }
+    public int YearOfStudy { get; set; }
+}
---
+++ Hort Daniel/L04/L04/StudentsService.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Data.Tables;
+using LanguageExt;
+
+namespace L04;
+
+public class StudentsService
+{
+    private readonly AzureStorageAccountSettings _settings;
+    private readonly TableClient _client;
+    public StudentsService(AzureStorageAccountSettings settings)
+    {
+        _settings = settings;
+        _client = new TableClient(new Uri(_settings.StorageUri), "Students",
+            new TableSharedKeyCredential(_settings.AccountName, _settings.StorageAccountKey));
+        _client.CreateIfNotExists();
+    }
+
+    public async Task<IEnumerable<StudentEntity>> GetAll()
+        => await _client.QueryAsync<StudentEntity>().AsEnumerable();
+
+    public TryAsync<StudentEntity> Get(string partitionKey, string rowKey)
+        => async () => (await _client.GetEntityAsync<StudentEntity>(partitionKey, rowKey)).Value;
+
+    public TryAsync<Unit> Add(StudentEntity student)
+        => async () => await _client.AddEntityAsync(student).ToUnit();
+
+    public TryAsync<Unit> Edit(StudentEntity student)
+        => async () => await _client.UpdateEntityAsync(student, student.ETag).ToUnit();
+
+    public TryAsync<Unit> Delete(string partitionKey, string rowKey)
+        => async () => await _client.DeleteEntityAsync(partitionKey, rowKey).ToUnit();
+}
