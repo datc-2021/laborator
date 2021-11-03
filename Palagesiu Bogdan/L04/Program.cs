94fa7d78e600849ea39cc9876566f49254d28c68
Palagesiu Alexandru-Bogdan <palagesiubogdan99@gmail.com>
2021-11-03 21:15:41 +0200

    Tema lab 4

---
+++ Palagesiu Bogdan/L04/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Models;
+
+namespace L04
+{
+    class Program
+    {
+        private static CloudTableClient tableClient;
+
+        private static CloudTable studentsTable;
+
+        static void Main(string[] args)
+        {
+            Task.Run(async () => { await Initialize(); })
+            .GetAwaiter()
+            .GetResult();
+        }
+
+        static async Task Initialize()
+        {
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=tema4pala;AccountKey=3VTefCba//yX6EJVvFEc8TdA55sx7OAVWgPXq5gAKt89gqW4NVxE/qeV3NJkPcdt/NddAjjbDbqjG8BOC1NpKA==;EndpointSuffix=core.windows.net";
+            var account = CloudStorageAccount.Parse(storageConnectionString);
+            tableClient = account.CreateCloudTableClient();
+
+            studentsTable = tableClient.GetTableReference("studenti");
+
+            await studentsTable.CreateIfNotExistsAsync();
+
+            await AddNewStudent();
+            await GetAllStudents();
+        }
+
+        private static async Task GetAllStudents()
+        {
+            Console.WriteLine("Universitate\tCNP\tNume\tEmail\tNumar telefon\tAn");
+            TableQuery<StudentEntity> query = new TableQuery<StudentEntity>();
+
+            TableContinuationToken token = null;
+            do
+            {
+                TableQuerySegment<StudentEntity> resultSegment = await studentsTable.ExecuteQuerySegmentedAsync(query, token);
+                token = resultSegment.ContinuationToken;
+
+                foreach (StudentEntity entity in resultSegment.Results)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2} {3}\t{4}\t{5}\t{6}", entity.PartitionKey, entity.RowKey, entity.FirstName, entity.LastName,
+                    entity.Email, entity.PhoneNumber, entity.Year);
+                }
+            } while (token != null);
+        }
+
+        private static async Task AddNewStudent()
+        {
+            var student = new StudentEntity("UPT", "1990809350012");
+
+            student.FirstName = "Marcus";
+            student.LastName = "Streltov";
+            student.Email = "marcusstreltov@gmail.com";
+            student.Year = 3;
+            student.PhoneNumber = "0728000000";
+            student.Faculty = "Kineto";
+
+            var insertOperation = TableOperation.Insert(student);
+
+            await studentsTable.ExecuteAsync(insertOperation);
+        }
+    }
+}
