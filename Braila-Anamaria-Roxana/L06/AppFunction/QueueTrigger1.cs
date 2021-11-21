5629daddfca3c2f69d4e36ad642071cf37e6e3d7
roxiexd7015 <57829914+roxiexd7015@users.noreply.github.com>
2021-11-21 23:02:09 +0200

    Tema 6

---
+++ Braila-Anamaria-Roxana/L06/AppFunction/QueueTrigger1.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Models;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class QueueTrigger1
+    {
+        [Function("QueueTrigger1")]
+        [TableOutput("studenti")]
+        public static StudentEntity Run([QueueTrigger("myqueue-items", Connection = "azurestorageroxie_STORAGE")] string myQueueItem,
+            FunctionContext context)
+        {
+            var logger = context.GetLogger("QueueTrigger1");
+            logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            var student = JsonConvert.DeserializeObject<StudentEntity>(myQueueItem);
+            return student;
+        }
+    }
+}
